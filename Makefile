26714b429dd6806e18b80c80bb09b394bfd5fead
Trevor Brown <admin@stratus3d.com>
2019-05-02 21:13:59 -0400

    Improve build

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: all compile watch release
 
-all: compile release
+all: release
 
 watch:
 	compass watch
@@ -11,6 +11,5 @@ compile:
 release: compile
 	mkdir -p build
 	cp -r img stylesheets build
-	asciidoctor -s -d book -o build/erlang_cheatsheet_content.html erlang_cheatsheet.adoc
 	# Generate HTML file
 	./build_erlang_cheatsheet.sh
--- build_erlang_cheatsheet.sh
+++ build_erlang_cheatsheet.sh
@@ -6,9 +6,12 @@ set -euo pipefail
 #ORIGINAL_IFS=$IFS
 IFS=$'\t\n' # Stricter IFS settings
 
+asciidoctor -s -d book -o build/erlang_cheatsheet_content.html erlang_cheatsheet.adoc
+
 (
-  export CHEATSHEET_TITLE="Erlang Cheatsheet"; \
-  export CHEATSHEET_HTML="$(cat build/erlang_cheatsheet_content.html)"; \
-  export CHEATSHEET_DESCRIPTION="The quirks of exit signals and exception handling you always forget"; \
+  export CHEATSHEET_TITLE CHEATSHEET_HTML CHEATSHEET_DESCRIPTION
+  CHEATSHEET_TITLE="Erlang Cheatsheet";
+  CHEATSHEET_HTML="$(cat build/erlang_cheatsheet_content.html)";
+  CHEATSHEET_DESCRIPTION="The quirks of exit signals and exception handling you always forget";
   envsubst < template.html > build/erlang_cheatsheet.html
 )
