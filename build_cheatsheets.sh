2227b2da5f976afa590446ace0956a7aa393cfc7
Trevor Brown <admin@stratus3d.com>
2019-05-18 17:57:08 -0400

    Begin work on OTP cheatsheet

--- build_cheatsheets.sh
+++ build_cheatsheets.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 #ORIGINAL_IFS=$IFS
 IFS=$'\t\n' # Stricter IFS settings
 
+# Erlang Cheatsheet
 asciidoctor -s -o build/erlang_cheatsheet_content.html erlang_cheatsheet.adoc
 
 (
@@ -14,4 +15,14 @@ asciidoctor -s -o build/erlang_cheatsheet_content.html erlang_cheatsheet.adoc
   CHEATSHEET_HTML="$(cat build/erlang_cheatsheet_content.html)";
   CHEATSHEET_DESCRIPTION="The quirks of exit signals and exception handling you always forget";
   envsubst < template.html > build/erlang_cheatsheet.html
+)
+
+# OTP Cheatsheet
+asciidoctor -s -o build/otp_cheatsheet_content.html otp_cheatsheet.adoc
+(
+  export CHEATSHEET_TITLE CHEATSHEET_HTML CHEATSHEET_DESCRIPTION
+  CHEATSHEET_TITLE="OTP Cheatsheet";
+  CHEATSHEET_HTML="$(cat build/otp_cheatsheet_content.html)";
+  CHEATSHEET_DESCRIPTION="";
+  envsubst < template.html > build/otp_cheatsheet.html
 )
